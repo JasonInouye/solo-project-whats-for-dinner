8aed7634055788206b713b1a4b1f8fa651abf101
Jason Inouye <jasonjinouye@gmail.com>
2022-04-18 15:25:00 -0500

    server router updated

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/dinner', userRouter);
+app.use('/dinner', dinnerRouter);
 
 // Serve static files
 app.use(express.static('build'));
