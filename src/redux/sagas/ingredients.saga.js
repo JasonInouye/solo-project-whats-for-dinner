654a60c0cad27f7392517734bf120f3a4c4fd2bb
Jason Inouye <jasonjinouye@gmail.com>
2022-04-22 00:04:16 -0500

    added pantry and spices sagas

--- src/redux/sagas/ingredients.saga.js
+++ src/redux/sagas/ingredients.saga.js
@@ -4,7 +4,6 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* getRefrigerator (){
     try {
         const refrigerator = yield axios.get('/api/ingredients/refrigerator');
-        console.log('SAGA GET REFRIGERATOR LOG', refrigerator.data);
         yield put({ type:'SET_REFRIGERATOR', payload: refrigerator.data });
 
     } catch (err){
@@ -12,8 +11,28 @@ function* getRefrigerator (){
     }   
 }
 
+function* getPantry (){
+    try {
+        const pantry = yield axios.get('/api/ingredients/pantry');
+        yield put({ type:'SET_PANTRY', payload: pantry.data });
+
+    } catch (err){
+        console.log('get all error', err);
+    }   
+}
+
+function* getSpices (){
+    try {
+        const spices = yield axios.get('/api/ingredients/spices');
+        yield put({ type:'SET_SPICES', payload: spices.data });
+
+    } catch (err){
+        console.log('get all error', err);
+    }   
+}
+
 function* addIngredient(action) {
-    console.log( 'here is the payload for my Add Ingredient', action.payload );
+    //console.log( 'here is the payload for my Add Ingredient', action.payload );
     try{
         yield axios.post('/api/ingredients', action.payload);
         yield put({ type: 'GET_REFRIGERATOR' })
@@ -35,6 +54,8 @@ function* deleteIngredient(action) {
 // This is for getting Ingredients for the refrigerator, pantry, spice
 function* getIngredients() {
     yield takeLatest('GET_REFRIGERATOR', getRefrigerator);
+    yield takeLatest('GET_PANTRY', getPantry);
+    yield takeLatest('GET_SPICES', getSpices);
     yield takeLatest('DELETE_INGREDIENT', deleteIngredient);
     yield takeLatest('ADD_REF_ITEM', addIngredient);
 }
