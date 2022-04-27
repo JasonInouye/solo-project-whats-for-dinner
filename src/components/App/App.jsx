5652b89a05bba81dfbe9ffd8168a9e75d11b8b63
Jason Inouye <jasonjinouye@gmail.com>
2022-04-27 14:08:46 -0500

    branching off to experiment with mui

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,9 +10,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
-
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
@@ -31,7 +29,10 @@ import SingleSearch from '../SingleSearch/SingleSearch';
 import SingleSearchResults from '../SingleSearch/SingleSearchResults';
 import StockSearch from '../StockSearch/StockSearch';
 import StockSearchResults from '../StockSearch/StockSearchResults';
+import SideBar from '../SideBar/SideBar';
 import './App.css';
+
+import { Box, Stack } from '@mui/material';
 
 function App() {
   const dispatch = useDispatch();
@@ -43,175 +44,178 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Router>
-      <div className='bg-image'>
-        <Nav />
-        <div className='main-container'>
-          <div>
-            <Switch>
-              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-              <Redirect exact from='/' to='/home' />
+    <Box>
+      <Router>
+        <div className='bg-image'>
+          <Nav />
+          <div className='main-container'>
+            <Stack direction='row' spacing={3} justifyContent='space-between'>
+              <SideBar />
+              <Switch>
+                {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+                <Redirect exact from='/' to='/home' />
 
-              {/* Visiting localhost:3000/about will show the about page. */}
-              <Route
-                // shows AboutPage at all times (logged in or not)
-                exact
-                path='/about'
-              >
-                <AboutPage />
-              </Route>
+                {/* Visiting localhost:3000/about will show the about page. */}
+                <Route
+                  // shows AboutPage at all times (logged in or not)
+                  exact
+                  path='/about'
+                >
+                  <AboutPage />
+                </Route>
 
-              {/* For protected routes, the view could show one of several things on the same route.
+                {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-              <ProtectedRoute
-                // logged in shows UserPage else shows LoginPage
-                exact
-                path='/user'
-              >
-                <UserPage />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows UserPage else shows LoginPage
+                  exact
+                  path='/user'
+                >
+                  <UserPage />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/info'
-              >
-                <InfoPage />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/info'
+                >
+                  <InfoPage />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/favorite'
-              >
-                <DinnerHomeTwo />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/favorite'
+                >
+                  <DinnerHomeTwo />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/search'
-              >
-                <SingleSearch />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/search'
+                >
+                  <SingleSearch />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/search/:searchItem'
-              >
-                <SingleSearchResults />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/search/:searchItem'
+                >
+                  <SingleSearchResults />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/stockSearch'
-              >
-                <StockSearch />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/stockSearch'
+                >
+                  <StockSearch />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/stockSearch/:searchString'
-              >
-                <StockSearchResults />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/stockSearch/:searchString'
+                >
+                  <StockSearchResults />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/schedule'
-              >
-                <WeeklySchedule />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/schedule'
+                >
+                  <WeeklySchedule />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/recipeDetails/:id'
-              >
-                <RecipeDetails />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/recipeDetails/:id'
+                >
+                  <RecipeDetails />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/ingredients/refrigerator'
-              >
-                <IngredientsRefrigerator />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/ingredients/refrigerator'
+                >
+                  <IngredientsRefrigerator />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/ingredients/pantry'
-              >
-                <IngredientsPantry />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/ingredients/pantry'
+                >
+                  <IngredientsPantry />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/ingredients/spices'
-              >
-                <IngredientsSpices />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/ingredients/spices'
+                >
+                  <IngredientsSpices />
+                </ProtectedRoute>
 
-              <ProtectedRoute
-                // logged in shows InfoPage else shows LoginPage
-                exact
-                path='/edit/:id'
-              >
-                <EditIngredient />
-              </ProtectedRoute>
+                <ProtectedRoute
+                  // logged in shows InfoPage else shows LoginPage
+                  exact
+                  path='/edit/:id'
+                >
+                  <EditIngredient />
+                </ProtectedRoute>
 
-              <Route exact path='/login'>
-                {user.id ? (
-                  // If the user is already logged in,
-                  // redirect to the /user page
-                  <Redirect to='/user' />
-                ) : (
-                  // Otherwise, show the login page
-                  <LoginPage />
-                )}
-              </Route>
+                <Route exact path='/login'>
+                  {user.id ? (
+                    // If the user is already logged in,
+                    // redirect to the /user page
+                    <Redirect to='/user' />
+                  ) : (
+                    // Otherwise, show the login page
+                    <LoginPage />
+                  )}
+                </Route>
 
-              <Route exact path='/registration'>
-                {user.id ? (
-                  // If the user is already logged in,
-                  // redirect them to the /user page
-                  <Redirect to='/user' />
-                ) : (
-                  // Otherwise, show the registration page
-                  <RegisterPage />
-                )}
-              </Route>
+                <Route exact path='/registration'>
+                  {user.id ? (
+                    // If the user is already logged in,
+                    // redirect them to the /user page
+                    <Redirect to='/user' />
+                  ) : (
+                    // Otherwise, show the registration page
+                    <RegisterPage />
+                  )}
+                </Route>
 
-              <Route exact path='/home'>
-                {user.id ? (
-                  // If the user is already logged in,
-                  // redirect them to the /user page
-                  <Redirect to='/user' />
-                ) : (
-                  // Otherwise, show the Landing page
-                  <LandingPage />
-                )}
-              </Route>
+                <Route exact path='/home'>
+                  {user.id ? (
+                    // If the user is already logged in,
+                    // redirect them to the /user page
+                    <Redirect to='/user' />
+                  ) : (
+                    // Otherwise, show the Landing page
+                    <LandingPage />
+                  )}
+                </Route>
 
-              {/* If none of the other routes matched, we will show a 404. */}
-              <Route>
-                <h1>404</h1>
-              </Route>
-            </Switch>
+                {/* If none of the other routes matched, we will show a 404. */}
+                <Route>
+                  <h1>404</h1>
+                </Route>
+              </Switch>
+            </Stack>
           </div>
+          {/* <Footer /> */}
         </div>
-        {/* <Footer /> */}
-      </div>
-    </Router>
+      </Router>
+    </Box>
   );
 }
 
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+function SideBar() {
+  return (
+    <Box bgcolor='red' flex={9}>SideBar</Box>
+  )
+}
+
+export default SideBar
