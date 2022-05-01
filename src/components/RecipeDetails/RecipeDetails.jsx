b4cbbab6ed80655b2e465837ee9445252b2d2865
Jason Inouye <jasonjinouye@gmail.com>
2022-05-01 13:36:40 -0500

    unable to get spinner on loading screen

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,12 +1,17 @@
-import { Button } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
+import { Button, Backdrop, CircularProgress } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function RecipeDetails() {
   let params = useParams();
   const [recipeDetails, setRecipeDetails] = useState({});
-  const [selectedButton, setSelectedButton] = useState("instructions");
+  const [selectedButton, setSelectedButton] = useState('instructions');
+  const [loading, setLoading] = (false)
+  const [open, setOpen] = useState(false)
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const fetchDetails = async () => {
     const data = await fetch(
@@ -19,51 +24,63 @@ function RecipeDetails() {
   };
 
   useEffect(() => {
+    setLoading(true)
+    setTimeout(()=> { setLoading(false)}, 1000)
     fetchDetails();
   }, [params.id]);
 
-  console.log("this is the JSON for details", recipeDetails);
-
   return (
-    <div className="main-container">
-      <div>
-        <h2>{recipeDetails.title}</h2>
-        <img src={recipeDetails?.image} alt="RecipeImage" />
-      </div>
+    <>
+      {recipeLoaded === true ? (
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={open}
+          onClick={handleClose}
+        >
+          <CircularProgress color='inherit' />
+        </Backdrop>
+      ) : (
       
-        {/* setting the active class based on which button is selected */}
-        <Button
-          variant="text"
-          className={selectedButton === "instructions" ? "active" : ""}
-          onClick={() => setSelectedButton("instructions")}
-        >
-          Instructions
-        </Button>
-        <Button
-          variant="text"
-          className={selectedButton === "ingredients" ? "active" : ""}
-          onClick={() => setSelectedButton("ingredients")}
-        >
-          Ingredients
-        </Button>
-        {selectedButton === "instructions" && (
+        <div className='main-container'>
           <div>
-            {/* this code sets the raw api data with html tags to render html data on dom */}
-            <p
-              dangerouslySetInnerHTML={{ __html: recipeDetails.instructions }}
-            ></p>
+            <h2>{recipeDetails.title}</h2>
+            <img src={recipeDetails?.image} alt='RecipeImage' />
           </div>
-        )}
-        {selectedButton === "ingredients" && (
-        <ul>
-          {recipeDetails.extendedIngredients?.map((ingredient) => {
-            return <li key={ingredient.id}>{ingredient.original}</li>;
-          })}
-        </ul>
-        )}
-    </div>
+
+          {/* setting the active class based on which button is selected */}
+          <Button
+            variant='text'
+            className={selectedButton === 'instructions' ? 'active' : ''}
+            onClick={() => setSelectedButton('instructions')}
+          >
+            Instructions
+          </Button>
+          <Button
+            variant='text'
+            className={selectedButton === 'ingredients' ? 'active' : ''}
+            onClick={() => setSelectedButton('ingredients')}
+          >
+            Ingredients
+          </Button>
+          {selectedButton === 'instructions' && (
+            <div>
+              {/* this code sets the raw api data with html tags to render html data on dom */}
+              <p
+                dangerouslySetInnerHTML={{ __html: recipeDetails.instructions }}
+              ></p>
+            </div>
+          )}
+          {selectedButton === 'ingredients' && (
+            <ul>
+              {recipeDetails.extendedIngredients?.map((ingredient) => {
+                return <li key={ingredient.id}>{ingredient.original}</li>;
+              })}
+            </ul>
+          )}
+        </div>
+      )}
+    </>
   );
 }
-
 
 export default RecipeDetails;
