b3f31e96f89f4f5087f0e9cbabb7174ca042430c
Jason Inouye <jasonjinouye@gmail.com>
2022-05-02 14:56:17 -0500

    update database.sql file

--- database.sql
+++ database.sql
@@ -8,3 +8,59 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+CREATE TABLE "public" (
+	"id" serial NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"access_level" varchar(10) NOT NULL
+);
+CREATE TABLE "ingredients_instock" (
+	"id" serial NOT NULL,
+	"ingredient" varchar(100) NOT NULL,
+	"quantity" int NOT NULL,
+	"unit" varchar(15) NOT NULL,
+	"location" varchar(255) NOT NULL,
+	"user_id" int NOT NULL
+);
+CREATE TABLE "weekly_plan" (
+	"id" serial NOT NULL,
+	"spoon_id" int NOT NULL,
+	"dow" varchar(50) NOT NULL,
+	"week_number" int NOT NULL,
+	"user_id" int NOT NULL
+);
+CREATE TABLE "recipes" (
+	"id" serial NOT NULL,
+	"spoon_id" int NOT NULL,
+	"recipe_name" varchar(255) NOT NULL,
+	"recipe_image" varchar(500) NOT NULL,
+	"likes" int NOT NULL,
+	"favorited" BOOLEAN NOT NULL DEFAULT 'false',
+	"user_id" int NOT NULL
+);
+CREATE TABLE "recipe_used_ingredients" (
+	"id" int NOT NULL,
+	"spoon_id" int NOT NULL,
+	"ingredient" varchar(100) NOT NULL,
+	"quantity" float4 NOT NULL,
+	"unit" varchar(15) NOT NULL,
+	"user_id" int NOT NULL
+);
+CREATE TABLE "recipe_missed_ingredients" (
+	"id" serial NOT NULL,
+	"spoon_id" serial NOT NULL,
+	"ingredient" varchar(100) NOT NULL,
+	"quantity" float4 NOT NULL,
+	"unit" varchar(255) NOT NULL,
+	"user_id" int NOT NULL
+);
+
+
+
+
+
+
+
+
+
+
